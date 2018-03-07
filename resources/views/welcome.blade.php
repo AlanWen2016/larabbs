5a2010cf00dd0fccaf4d08ad64ef42c09f1f43f0
AlanWen2016[C <906075319@qq.com>
2018-03-07 14:38:36 +0800

    Adjuct configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    biubiubiu,dadada
                 </div>
 
                 <div class="links">
